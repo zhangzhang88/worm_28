ff70feae78b57b3ece7a90318c5cbb97e8af24c2
zhangzhang88 <68057668+zhangzhang88@users.noreply.github.com>
2025-11-21 16:19:45 +0800

    Update course data

--- data/courses/index.ts
+++ data/courses/index.ts
@@ -1,4 +1,5 @@
 import { LessonConfig } from '../types';
+import { sentences as courseId1Lesson1 } from './courseId_1/lesson1';
 
 interface CourseConfig {
   courseId: string;
@@ -6,7 +7,21 @@ interface CourseConfig {
   lessons: LessonConfig[];
 }
 
+const courseId1Lessons: LessonConfig[] = [
+  {
+    lessonNumber: 1,
+    title: 'Lesson 1 - 基础英语句子学习',
+    sentences: courseId1Lesson1
+  },
+];
+
 export const courseConfigs: Record<string, CourseConfig> = {
+
+  courseId_1: {
+    courseId: 'courseId_1',
+    title: '零基础学英语',
+    lessons: courseId1Lessons
+  },
 };
 
 export const getCourseById = (courseId: string): CourseConfig | undefined =>
